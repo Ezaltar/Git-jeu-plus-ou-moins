b514f54c5d1d5aee9f8121817763a28cb6a2bd73
UTILISATEUR <UTILISATEUR@192.168.1.166>
2017-07-25 14:01:46 +0200

    Le jeu est terminé

--- public_html/scriptJeuPlusMoins.js
+++ public_html/scriptJeuPlusMoins.js
@@ -1,29 +1,29 @@
 
 function nombre(min, max) {
-    var random = (Math.floor((20 - 10) * Math.random()) + 10);
     var compteur = 0;
-    alert(random);
+    var compteur2 = 0;
     do {
-    var message = prompt("Entrer un nombre entre 10 et 20");
-    compteur++;
-    
-        if (random > message) {
-            alert("Ce chiffre est trop bas");
-            
-            
+        compteur2++;
+        var random = (Math.floor((20 - 10) * Math.random()) + 10);
 
-        } else if (random < message) {
-            alert("Ce chiffre Est trop élever");
-            
-            
+        alert(random);
+        do {
+            var message = prompt("Entrer un nombre entre 10 et 20");
+            compteur++;
+            if (random > message) {
+                alert("Ce chiffre est trop bas");
+            } else if (random < message) {
+                alert("Ce chiffre Est trop élever");
+            }
+        } while (random != message);
+        alert("T'es le meilleur ! Tu as fini en " + compteur + " coups " + "et tu as fait " + compteur2 + " parties");
+
+
+        var t = confirm("Voulez vous rejouez ?");
+        if (t == true) {
+        } else {
+            alert("A bientôt");
         }
-    } while (random != message);
-    
-    
-    alert("T'es le meilleur ! Tu as fini en " + compteur);
+    } while (t == true);
 }
-
-
-
-
-
+    
