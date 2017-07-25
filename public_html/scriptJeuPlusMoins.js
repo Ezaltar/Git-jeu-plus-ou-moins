c40ff62cea3e6644f5e8761eedd29243ef5ced55
UTILISATEUR <UTILISATEUR@192.168.1.166>
2017-07-25 11:51:55 +0200

    Le jeu fonctionne: on peut chercher le nombre mystère et nous donne le nombre de tentative pour trouver ce dernier

--- public_html/scriptJeuPlusMoins.js
+++ public_html/scriptJeuPlusMoins.js
@@ -1,17 +1,29 @@
 
 function nombre(min, max) {
+    var random = (Math.floor((20 - 10) * Math.random()) + 10);
+    var compteur = 0;
+    alert(random);
+    do {
     var message = prompt("Entrer un nombre entre 10 et 20");
-    var random = Math.floor((max - min) * Math.random() + 1);
-   // compteur = 0 nombre d'essai//
+    compteur++;
     
-    if (random > message){
-    alert("Ce chiffre est trop bas");
-            return;
-    } else if (random < message){
-    alert("Ce chiffre Est trop élever");
-            return;
-    }
+        if (random > message) {
+            alert("Ce chiffre est trop bas");
+            
+            
 
-    }
+        } else if (random < message) {
+            alert("Ce chiffre Est trop élever");
+            
+            
+        }
+    } while (random != message);
+    
+    
+    alert("T'es le meilleur ! Tu as fini en " + compteur);
+}
+
+
+
 
 
